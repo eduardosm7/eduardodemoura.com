0cbadb6747aeb0b31e77aa1619858b4559d5bf67
eduardothemoura <eduardothemoura@gmail.com>
2022-12-23 21:51:21 +0100

    Remove HomepageFeatures

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import {useColorMode} from '@docusaurus/theme-common';
 
 import styles from './index.module.css';
@@ -36,9 +35,6 @@ export default function Home() {
       title={``}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
     </Layout>
   );
 }
